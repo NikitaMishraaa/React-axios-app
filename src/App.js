c7c7fe1a8288db40ae51b82672f804bd7184fab4
NikitaMishraaa <nikitarm1997@gmail.com>
2021-10-15 14:52:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+import Header from './Components/Header';
+import Maincontent from './Components/Maincontent';
+
+function App() {
+  return (
+    <div>
+     <Header/>
+     <Maincontent/>
+     
+    </div>
+  );
+}
+
+export default App;
